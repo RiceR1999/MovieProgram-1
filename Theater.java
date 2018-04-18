f736e21ddff5e72a8c970cb7a76c9cb8961320dc
RiceR1999 <38504074+RiceR1999@users.noreply.github.com>
2018-04-18 13:44:31 -0400

    Update Theater.java

--- Theater.java
+++ Theater.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-
+/**
+* Class that defines a theater object
+* @version 4.18.2018
+* @author
+*/
 public class Theater 
 {
-
+ 
+  private String theaterName;
+  private ArrayList<Show> shows;
+	
+	/**
+	* Constructor for theater class
+	*/
 	public Theater() 
 	{
 
-	}
-	/*
-	* Method Responsibe for displaying show information
-	* and taking using input.
-	/*
-	public void start()
-	{
-		
 	}
 }
