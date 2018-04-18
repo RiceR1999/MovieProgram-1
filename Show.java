3717c731157142c6e56d7acbb250df3c70af3174
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2018-04-18 13:19:41 -0400

    Update Show.java

--- Show.java
+++ Show.java
@@ -2,19 +2,26 @@
 public class Show 
 {
 
-	public Show() 
+	public Show(int time,int day,int month,int year,int showName,int capacity) 
 	{
 		// TODO Auto-generated constructor stub
 	}
-//int time
+int time;
 	
-//int day
-//int month
-//int year
-	
-//int date
-	
-//String showName
-//int capacity 
+int day;
+int month;
+int year;
 
+String showName;
+int capacity ;
+
+//One ArrayList for each Row.
+ArrayList<Seat> RowA= new ArrayList<Seat>();
+ArrayList<Seat> RowB= new ArrayList<Seat>();
+/*
+* Method that fills Row ArrayLists with seat objects.
+*/
+public void fillRow(ArrayList given)
+{
+	
 }
