8a97ea7454e697beefacea6feb371698a46fdfe3
RiceR1999 <38504074+RiceR1999@users.noreply.github.com>
2018-04-18 14:06:03 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,10 +1,14 @@
-
+/**
+* Driver class for main method
+* @version 4.18.2018
+* @author Zach,Tavis,Zain,Nick,Ryan
+*/
 public class Driver {
-
-	public Driver() {
-		// TODO Auto-generated constructor stub
-	}
-
+ 
+	/**
+	* Main method that initiates the program
+	* @param args
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
