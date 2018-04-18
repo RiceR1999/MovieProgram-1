b5d2b4d97477db4c8fcf714cbca510a6e68fa309
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2018-04-18 13:52:19 -0400

    Update CinemaBookingSystem.java

--- CinemaBookingSystem.java
+++ CinemaBookingSystem.java
@@ -1,10 +1,20 @@
-
+/**
+* Class that contains the start method.
+* @version 4.18.2018
+* @author Tavis Baylor, Zach Ventola, Ryan Rice, Zain Shahabuddin, Nick DiGiovanni
+*/
 public class CinemaBookingSystem 
 {
 
 	public CinemaBookingSystem() 
 	{
-		// TODO Auto-generated constructor stub
+		//constructor
 	}
-
+	/*
+	* Method responisble for displaying Theaters, Shows, and taking user input.
+	*/
+	public void start()
+	{
+		
+	}
 }
