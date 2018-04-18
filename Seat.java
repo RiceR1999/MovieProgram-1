2fe86e92fa7cfe77a34fabd47780b941389a9008
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2018-04-18 13:04:37 -0400

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -9,8 +9,26 @@ public class Seat
 boolean taken;
 Customer theCustomer;
 	
+	/*
+	* Method that checks if a seat is taken.
+	*/
 	public boolean isTaken()
 	{
 		return taken;
+	}
+	/*
+	* Method that adds cutomer to a seat.
+	*/
+	public void addCustomer(Customer c)
+	{
+		theCustomer = c;
+	}
+	/*
+	* Method that gets customer info.
+	*/
+	public void getCustomerInfo(Customer c)
+	{
+		theCustomer.getName();
+		theCustomer.getNumber();
 	}
 }
