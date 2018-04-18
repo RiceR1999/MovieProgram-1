33010f94ed93a929295378cd558d2d9855d71720
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2018-04-18 12:53:59 -0400

    Add files via upload

---
+++ Show.java
@@ -0,0 +1,20 @@
+
+public class Show 
+{
+
+	public Show() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+//int time
+	
+//int day
+//int month
+//int year
+	
+//int date
+	
+//String showName
+//int capacity 
+
+}
