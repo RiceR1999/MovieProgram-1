e63e8561cf8f3b0a714bf7e949032586218cc17a
Ryan Rice <38504074+RiceR1999@users.noreply.github.com>
2018-05-22 21:38:57 -0400

    Update Show.java

--- Show.java
+++ Show.java
@@ -1,22 +1,26 @@
+import java.util.*;
 /**
 * Class that defines a show object
 * @version 4.18.2018
-* @author Nick Digi
+* @author Tavis Baylor, Zach Ventola, Ryan Rice, Zain Shahabuddin, Nick DiGiovanni
 */
 public class Show 
 {
-	int time;
+	String time;
 	
 	int day;
 	int month;
 	int year;
-	
+	int ID;
 	String showName;
-	int capacity ;
+	//int capacity ;
 
 	//One ArrayList for each Row.
-	ArrayList<Seat> RowA= new ArrayList<Seat>();
-	ArrayList<Seat> RowB= new ArrayList<Seat>();
+	ArrayList<Long> RowA= new ArrayList<Long>();
+	ArrayList<Long> RowB= new ArrayList<Long>();
+	ArrayList<Long> RowC= new ArrayList<Long>();
+	ArrayList<Long> RowD= new ArrayList<Long>();
+	
 	
 	/**
 	* Constructor for Show class
@@ -27,23 +31,124 @@ public class Show
 	* @param showName
 	* @param capacity
 	*/
-	public Show(int time,int day,int month,int year,String showName,int capacity) 
+	public Show(String thetime,int theday,int themonth,int theyear,String theshowName,int theID) 
 	{
-		// TODO Auto-generated constructor stub
+		time = thetime;
+		year = theyear;
+		day= theday;
+		month = themonth;
+		year = theyear;
+		showName = theshowName;
+		ID = theID;
+		fillRow(RowA);
+		fillRow(RowB);
+		fillRow(RowC);
+		fillRow(RowD);
 	}
 
 /**
 * Method that fills Row ArrayLists with seat objects.
 * @param given
 */
-public void fillRow(ArrayList given)
+public void fillRow(ArrayList<Long> given)
 {
-	
+	int index = 0;
+	while(index<10)
+	{
+		given.add(new Long(0));
+		index++;
+	}
 }
 /**
 * Method that prints time, date, and show name to user.
 */
+public void getRowSeats()
+{
+	int index = 0;
+	System.out.print("ROW A: ");
+	while(index<10)
+	{
+		if(RowA.get(index)==0)
+		{
+			System.out.print("[Seat  " + (index+1)+"]");
+		}
+		else
+		{
+			System.out.print("[TAKEN " + (index+1)+"]");
+		}
+		index++;
+	}
+	System.out.println("");
+	System.out.print("ROW B: ");
+	index = 0;
+	while(index<10)
+	{
+		if(RowB.get(index)==0)
+		{
+			System.out.print("[Seat  " + (index+1)+"]");
+		}
+		else
+		{
+			System.out.print("[TAKEN " + (index+1)+"]");
+		}
+		index++;
+	}
+	System.out.println("");
+	System.out.print("ROW C: ");
+	index = 0;
+	while(index<10)
+	{
+		if(RowC.get(index)==0)
+		{
+			System.out.print("[Seat  " + (index+1)+"]");
+		}
+		else
+		{
+			System.out.print("[TAKEN " + (index+1)+"]");
+		}
+		index++;
+	}
+	System.out.println("");
+	System.out.print("ROW D: ");
+	index = 0;
+	while(index<10)
+	{
+		if(RowD.get(index)==0)
+		{
+			System.out.print("[Seat  " + (index+1)+"]");
+		}
+		else
+		{
+			System.out.print("[TAKEN " + (index+1)+"]");
+		}
+		index++;
+	}
+	System.out.println("");
+}
 public void getInfo()
 {
-	
+	System.out.println(showName + ": Playing on " + day + "/" + month + "/" + year + ": At "+ time);
+	System.out.println("ID Number: " + ID);
+}
+public void reserveASeat(String theRow, String theSeat, Long number)
+{
+	int seatChoice = Integer.parseInt(theSeat)-1;
+	if(theRow.equals("A"))
+	{
+		RowA.set(seatChoice, number);
+	}
+	if(theRow.equals("B"))
+	{
+		RowB.set(seatChoice, number);
+	}
+	if(theRow.equals("C"))
+	{
+		RowC.set(seatChoice, number);
+	}
+	if(theRow.equals("D"))
+	{
+		RowD.set(seatChoice, number);
+	}
+}
+
 }
